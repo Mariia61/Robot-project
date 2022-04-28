85d4003d2966d56ce81360f0e2fdf54530812730
Mariia Kancheva <101674696+Mariia61@users.noreply.github.com>
2022-04-28 22:12:12 +0300

    update index.html and js file

--- JS Exercise_1_2.js
+++ JS Exercise_1_2.js
@@ -14,16 +14,34 @@ function assign_phrase() {
 
 load.addEventListener("load", assign_phrase);
 */
+
+const robots = [];
+
+
 const form = document.querySelector("#robot-form");
+const phrase = form.querySelector("comment");
 form.addEventListener("createNewRobot", createRobot);
+
+
+
+function onCheckBoxTalkChecked() {
+    const checkbox = form.querySelector("input[name ='checkboxTalk']");
+    phrase.disabled = checkbox.checked;
+}
+
 
 function createRobot(event) {
     const name = form.querySelector("input[name ='robot-name']");
     const type = form.querySelector(".drop-downs-options select").value;
     const color = form.querySelector("input[name ='color-picker']");
-    robot1.robot_name = name;
-    robot1.color = color;
-    robot1.type = type;
+
+
+    let robotN;
+    robotN.robot_name = name;
+    robotN.color = color;
+    robotN.type = type;
+    robots.push(robotN);
+
     event.preventDefault();
 
 }
