936bac8bbeeeb488f7f4d111da170c21e1ba6b24
Mariia Kancheva <101674696+Mariia61@users.noreply.github.com>
2022-05-03 00:23:42 +0300

    update index.html and js file

--- JS Exercise_1_2.js
+++ JS Exercise_1_2.js
@@ -1,47 +1,67 @@
-let phrases = ["Hello", "Nice to meet you", "How are you "];
+/* 
+var randomWords = ["Hello", "Nice to meet you", "How are you"];
 
 function timeFunction() {
-    setTimeout(function () { alert(phrases[2]); }, 2000);
+   setTimeout(function () { alert(randomWords[Math.floor(Math.random() * 3)]); }, 2000);
 }
 
 let robot1 = { robot_name, color, type };
-/*
+
 var load = document.getElementById("body");
 
 function assign_phrase() {
-    robot2.phrase = "Hello, " + robot2.robot_name + " !";
+   robot2.phrase = "Hello, " + robot2.robot_name + " !";
 }
 
 load.addEventListener("load", assign_phrase);
 */
 
-const robots = [];
+var robots = [];
 
+var createBtn = document.getElementById("createNewRobotButton");
+createBtn.addEventListener("click", createRobot);
+createBtn.addEventListener("click", showRobots);
 
-const form = document.querySelector("#robot-form");
-const phrase = form.querySelector("comment");
-form.addEventListener("createNewRobot", createRobot);
-
-
-
+//method for disabling and enabling the phrase input field
 function onCheckBoxTalkChecked() {
-    const checkbox = form.querySelector("input[name ='checkboxTalk']");
-    phrase.disabled = checkbox.checked;
+    const checkbox = document.querySelector('#checkbox-talk');
+    if (checkbox.checked) {
+        const phrase = document.querySelector('#phrase');
+        phrase.disabled = false;
+    } else {
+        phrase.disabled = true;
+    }
 }
 
 
-function createRobot(event) {
-    const name = form.querySelector("input[name ='robot-name']");
-    const type = form.querySelector(".drop-downs-options select").value;
-    const color = form.querySelector("input[name ='color-picker']");
+function createRobot() {
+    const name = form.querySelector('#robot-name');
+    var select = document.getElementById('robot-type');
+    var type = select.options[select.selectedIndex].value;
+    var color = document.getElementById('#color-picker').value;
 
+    var robotN = {
+        robot_name: name,
+        color: color,
+        gender: type
+    };
 
-    let robotN;
-    robotN.robot_name = name;
-    robotN.color = color;
-    robotN.type = type;
     robots.push(robotN);
 
-    event.preventDefault();
+    // event.preventDefault();
 
 }
+
+function showRobots() {
+    console.log(robots);
+}
+
+function hideSection() {
+
+    const roboSection = document.querySelector('#robotLoadSection');
+    roboSection.style.display = "none";
+}
+
+
+
+
